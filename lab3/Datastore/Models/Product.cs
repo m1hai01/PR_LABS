91aefac07e90734ec11aee0d0c446b71358a9878
m1hai01 <mustucmihai78@gmail.com>
2022-11-29 16:25:54 +0200

    Create the model

---
+++ lab3/Datastore/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Datastore.Models
+{
+    public class Product
+    {
+        [JsonIgnore]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+    }
+}
